28484f8521ced1cddd86fd31bec620ede61196bc
RaihanManush <raihanchow420dhury@gmail.com>
2025-06-30 11:40:44 +0600

    Create TaskTwo.java

---
+++ LabOne/TaskTwo.java
@@ -0,0 +1,11 @@
+class Main
+{
+public static void main(String[] args) {
+String name = "Raihan chowdhury";
+String studentId = "E243030";
+String email = "raihanchow420dhury@gmail.com";
+String number = "01972896180";
+String address = "Chawk bazar";
+System.out.println("Name: " + name +"\nStudent Id: "+ studentId + "\nEmail: "+email +"\nPhone: "+ number +"\nAddress: "+ address);
+}
+}
